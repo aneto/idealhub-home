05dfcb19ff061e44ed0c7638adc5d1caa4d21698
Alexandre Antoniu Neto <alexandreantoniu@hotmail.com>
2016-10-18 02:13:58 -0200

    Slider - Files
    Ajustei os arquivos por areas, arrumei alguns textos e inseri o
    carrossel de imagens abaixo do banner principal!

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Aumente suas vendas com os maiores marketplaces do mercado. A integração ideal para a sua plataforma e-commerce utiliza tecnologia de ponta para suportar grandes volumes de dados.">
+    <meta name="author" content="iDeal HUB">
+
+    <title>iDeal HUB - Integrador Marketplace - B2W, Walmart, Extra, Mercado Livre</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Theme CSS -->
+    <link href="css/agency.css" rel="stylesheet">
+</head>
+
+<body id="page-top" class="index">
+
+    <?php
+    include("pages/menu.php");
+    include("pages/cabecalho.php");
+    include("pages/recursos.php");
+    include("pages/marketplaces.php");
+    include("pages/sobre.php");
+    include("pages/faleConosco.php");
+    include("pages/rodape.php");
+    include("pages/modals.php");
+    ?>
+
+    <!-- jQuery -->
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/agency.min.js"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#myCarousel').carousel({
+                interval: 10000
+            })
+            $('.fdi-Carousel .item').each(function () {
+                var boxCount = 4; // change this to how many box you want
+                var next = $(this).next();
+                for(i = 1; i < boxCount; i++) {
+                    if (!next.length) {
+                        next = $(this).siblings(':first');
+                    }
+                    next.children(':first-child').clone().appendTo($(this));
+                    next = next.next();
+                }
+            });
+        });
+
+    </script>>
+
+</body>
+
+</html>
---
+++ pages/cabecalho.php
@@ -0,0 +1,86 @@
+    <header>
+        <div class="container">
+            <div class="intro-text">
+                <div class="intro-lead-in">Economize tempo e dinheiro!</div>
+                <div class="intro-heading">Experimente o novo e descubra o IDEAL!</div>
+                <a href="#services" class="page-scroll btn btn-xl">Conheça!</a>
+            </div>
+        </div>
+    </header>
+
+    <aside class="clients">
+        <div class="container">
+            <div class="row">
+                <div id="myCarousel" class="carousel fdi-Carousel slide">
+                    <div class="carousel fdi-Carousel slide" id="eventCarousel" data-interval="0">
+                        <div class="carousel-inner onebyone-carosel">
+                            <div class="item active">
+                                <div class="col-md-3 col-sm-6">
+                                    <img src="img/mktplaces/americanas.png" class="img-responsive center-block" alt="Americanas">
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="col-md-3 col-sm-6">
+                                    <img src="img/mktplaces/buscape.png" class="img-responsive center-block" alt="Buscapé">
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="col-md-3 col-sm-6">
+                                    <img src="img/mktplaces/casas-bahia.png" class="img-responsive center-block" alt="Casas Bahia">
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="col-md-3 col-sm-6">
+                                    <img src="img/mktplaces/dafiti.png" class="img-responsive center-block" alt="Dafiti">
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="col-md-3 col-sm-6">
+                                    <img src="img/mktplaces/extra.png" class="img-responsive center-block" alt="Extra">
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="col-md-3 col-sm-6">
+                                    <img src="img/mktplaces/google.png" class="img-responsive center-block" alt="Google">
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="col-md-3 col-sm-6">
+                                    <img src="img/mktplaces/mercado-livre.png" class="img-responsive center-block" alt="Mercado Livre">
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="col-md-3 col-sm-6">
+                                    <img src="img/mktplaces/netshoes.png" class="img-responsive center-block" alt="Netshoes">
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="col-md-3 col-sm-6">
+                                    <img src="img/mktplaces/ponto-frio.png" class="img-responsive center-block" alt="Ponto Frio">
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="col-md-3 col-sm-6">
+                                    <img src="img/mktplaces/shoptime.png" class="img-responsive center-block" alt="Shoptime">
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="col-md-3 col-sm-6">
+                                    <img src="img/mktplaces/submarino.png" class="img-responsive center-block" alt="Submarino">
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="col-md-3 col-sm-6">
+                                    <img src="img/mktplaces/walmart.png" class="img-responsive center-block" alt="Walmart">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="btnChange">
+                            <a class="left carousel-control" href="#eventCarousel" data-slide="prev" alt="prev"></a>
+                            <a class="right carousel-control" href="#eventCarousel" data-slide="next" alt="next"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </aside>
