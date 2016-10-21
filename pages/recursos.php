fe057d3a8382b700bda3dbf7c275cd327f7a34f5
iohangp <iohangp@gmail.com>
2016-10-21 00:12:32 -0200

    Criação de conteúdos
    Criado os conteúdos da pagina de Recursos.

--- pages/recursos.php
+++ pages/recursos.php
@@ -3,59 +3,71 @@
             <div class="row">
                 <div class="col-lg-12 text-center">
                     <h2 class="section-heading">Recursos</h2>
-                    <h3 class="section-subheading text-muted">Ferramentas para auxiliar seu gerenciamento!</h3>
+                    <h3 class="section-subheading text-muted">
+                    <p>O iDeal Hub é uma ferramenta que centraliza todas as operações de Marketplace em um único ambiente. </p>
+                    <p>    
+                    Gerencie cadastro de produtos, vendas, estoque e integrações para vários marketplaces simultaneamente e economize tempo e dinheiro. 
+                    </p>
+                    <p>
+                        Se sua empresa procura uma ferramenta eficiente e prática para otimizar sua gestão, o iDeal Hub atende a sua necessidade.
+                    </p>
+                    <p>
+                        Conheça abaixo um pouco sobre a nossa ferramenta!
+                    </p>
+
+                     </h3>
+                </div>
+            </div>
+            <div class="row text-center">              
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="glyphicon glyphicon-barcode fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4 class="service-heading">Produtos</h4>
+                    <p class="text-muted">Cadastre, importe ou integre o seu mix produtos e tenha tudo em um só lugar sem precisar de outra ferramenta. </p>
+                </div>
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="glyphicon glyphicon-usd fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4 class="service-heading">Pedidos</h4>
+                    <p class="text-muted">Acompanhe suas vendas em tempo real em todos os marketplaces, receba notificações, e gerencie os pedidos de forma centralizada .</p>
+                </div>
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="glyphicon glyphicon-refresh fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4 class="service-heading">Integração</h4>
+                    <p class="text-muted">Contecte seu e-commerce ou ERP com o iDeal Hub e automatize ainda mais seus processos. Nós fazemos o trabalho pesado para você.</p>
                 </div>
             </div>
             <div class="row text-center">
                 <div class="col-md-4">
                     <span class="fa-stack fa-4x">
                         <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
+                        <i class="glyphicon glyphicon-list-alt fa-stack-1x fa-inverse"></i>
                     </span>
-                    <h4 class="service-heading">E-Commerce</h4>
-                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                    <h4 class="service-heading">Relatórios</h4>
+                    <p class="text-muted">O iDeal Hub disponibiliza relatórios completos com informações detalhadas de vendas e produtos para te auxiliar nas decisões estratégicas do seu negócio.</p>
                 </div>
                 <div class="col-md-4">
                     <span class="fa-stack fa-4x">
                         <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-laptop fa-stack-1x fa-inverse"></i>
+                        <i class="glyphicon glyphicon-send fa-stack-1x fa-inverse"></i>
                     </span>
-                    <h4 class="service-heading">Responsive Design</h4>
-                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                    <h4 class="service-heading">Frete</h4>
+                    <p class="text-muted">Gerencie suas regras de frete de maneira simples. Com módulo de integração direta com os correios.</p>
                 </div>
                 <div class="col-md-4">
                     <span class="fa-stack fa-4x">
                         <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-lock fa-stack-1x fa-inverse"></i>
+                        <i class="glyphicon glyphicon-phone fa-stack-1x fa-inverse"></i>
                     </span>
-                    <h4 class="service-heading">Web Security</h4>
-                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-                </div>
-            </div>
-            <div class="row text-center">
-                <div class="col-md-4">
-                    <span class="fa-stack fa-4x">
-                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
-                    </span>
-                    <h4 class="service-heading">E-Commerce</h4>
-                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-                </div>
-                <div class="col-md-4">
-                    <span class="fa-stack fa-4x">
-                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-laptop fa-stack-1x fa-inverse"></i>
-                    </span>
-                    <h4 class="service-heading">Responsive Design</h4>
-                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-                </div>
-                <div class="col-md-4">
-                    <span class="fa-stack fa-4x">
-                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-lock fa-stack-1x fa-inverse"></i>
-                    </span>
-                    <h4 class="service-heading">Web Security</h4>
-                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                    <h4 class="service-heading">Mobilidade</h4>
+                    <p class="text-muted">Acesse seu painel e tenha as informações a qualquer hora e lugar e a partir de qualquer dispositivo.</p>
                 </div>
             </div>
         </div>
