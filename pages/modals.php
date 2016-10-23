be252bd9390922216623aba7e6a6528302f4feb2
Alexandre Antoniu Neto <alexandreantoniu@hotmail.com>
2016-10-23 19:39:42 -0200

    modals
    ajuste das cores e texto dos modals

--- pages/modals.php
+++ pages/modals.php
@@ -25,3 +25,58 @@
         </div>
     </div>
 </div>
+
+<div class="portfolio-modal modal fade" id="americanas" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <div class="modal-body">
+                            <h2>Americanas.com</h2>
+                            <p id="subtitle" class="item-intro text-muted">Faça parte da maior!</p>
+                            <img class="img-responsive img-centered" src="img/portfolio/americanas-modal.jpg" alt="">
+                            <p>Com o Marketplace da <a href="http://www.americanas.com.br/marketplace">Americanas.com</a>, você expõe seus produtos na maior loja online da América Latina e atinge um número muito maior de clientes qualificados, além de contar com a tecnologia, a segurança e o atendimento muito bem prezados por esse parceiro.</p>
+
+                            <p><a target="_blank" href="http://www.americanas.com.br/marketplace">Clique aqui</a> para visualizar a pagina oficial da Americanas!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="portfolio-modal modal fade" id="walmart" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <div class="modal-body">
+                            <h2>Walmart</h2>
+                            <p id="subtitle" class="item-intro text-muted">Mais de 10 milhões de clientes cadastrados!</p>
+                            <img class="img-responsive img-centered" src="img/portfolio/walmart-modal.png" alt="">
+                            <p>Venda seus produtos com o maior varejista do mundo. O Marketplace do <a href="https://marketplace.walmart.com.br">Walmart</a> foi desenvolvido pensando em aumentar suas vendas! Seu modelo de negócio muito bem estruturado faz suas ofertas serem exibidas pelos milhões de clientes que trafegam no Walmart.
+                            </p>
+
+                            <p><a target="_blank" href="https://marketplace.walmart.com.br">Confira aqui</a> a pagina oficial do Walmart!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
